d4bfc21207b3da7424896add40e002b77d8e90c7
unknown <jb53807@fer.hr>
2024-11-09 23:33:02 +0100

    commit

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,5 +1,9 @@
 import express, {Request, Response, Router} from 'express';
 import bcryptjs from 'bcryptjs';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const router: Router = express.Router();
 let xssEnabled = false;
@@ -7,6 +11,19 @@ let sdeEnabled = false;
 
 let username: string = "";
 let password: string = "";
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: 5432,
+    ssl: true
+});
+
+pool.connect()
+    .then(() => console.log("Spojena baza."))
+    .catch(err => console.error("Greška: ", err));
 
 router.get("/", (req: Request, res: Response) => {
     const message = req.query.message || '';
@@ -35,12 +52,27 @@ router.post('/login', async(req: Request, res: Response) => {
 
     if (!sdeEnabled) {
         const hashed = await bcryptjs.hash(pass, 10);
-        username = user;
-        password = hashed;
+       
+        try {
+            await pool.query(
+                'INSERT INTO korisnici (username, password) VALUES ($1, $2)',
+                [user, hashed]
+            );
+        } catch (error) {
+            console.error("Greška:", error);
+        }
     } else {
-        username = user;
-        password = pass;
+        try {
+            await pool.query(
+                'INSERT INTO korisnici (username, password) VALUES ($1, $2)',
+                [user, pass]
+            );
+        } catch (error) {
+            console.error("Greška:", error);
+        }
     }
+    username = user;
+    password = pass;
 
     res.redirect("/home");
 });
